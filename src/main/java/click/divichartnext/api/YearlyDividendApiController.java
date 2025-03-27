97b3ed77e89c36c20063f131e899069d014ff4bb
takuya-fukumura <blackchoco1114@gmail.com>
2025-03-27 23:06:23 +0900

    API追加

---
+++ src/main/java/click/divichartnext/api/YearlyDividendApiController.java
@@ -0,0 +1,45 @@
+package click.divichartnext.api;
+
+import click.divichartnext.bean.dto.YearlyDividendDto;
+import click.divichartnext.service.YearlyDividendService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * 年別配当データを提供するAPI
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api/yearlyDividend")
+@CrossOrigin(origins = "http://localhost:3000") // Next.jsのURLに変更
+public class YearlyDividendApiController {
+    private static final int NUM_OF_YEARS = 5;
+
+    private final YearlyDividendService service;
+
+    @Autowired
+    public YearlyDividendApiController(YearlyDividendService yearlyDividendService) {
+        this.service = yearlyDividendService;
+    }
+
+    /**
+     * グラフ用のJSONデータを提供
+     */
+    @GetMapping
+    public YearlyDividendDto getYearlyDividend(@AuthenticationPrincipal UserDetails user) {
+        log.debug("年別配当データを取得");
+
+        List<Integer> pastYears = service.getLastNYears(NUM_OF_YEARS);
+        String labels = service.createYearLabels(pastYears);
+        List<BigDecimal> yearlyDividendData = service.getYearlyDividendData(pastYears, user.getUsername());
+        String chartData = service.createChartData(yearlyDividendData);
+
+        return new YearlyDividendDto(labels, chartData);
+    }
+}
