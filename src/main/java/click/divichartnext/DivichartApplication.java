53dac1aa67ba46ddd7f56002c773ff64e2d33150
takuya-fukumura <blackchoco1114@gmail.com>
2025-04-18 00:07:02 +0900

    PageImplインスタンスを直接JSONとしてシリアライズする警告に対応

--- src/main/java/click/divichartnext/DivichartApplication.java
+++ src/main/java/click/divichartnext/DivichartApplication.java
@@ -2,8 +2,10 @@ package click.divichartnext;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class DivichartApplication {
 
     public static void main(String[] args) {
