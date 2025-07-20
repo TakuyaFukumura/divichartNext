17b49aa3df22cc4151504147c53493591eaa1488
takuya-fukumura <blackchoco1114@gmail.com>
2025-07-21 00:39:46 +0900

    整形

--- src/test/java/click/divichartnext/util/JwtUtilTest.java
+++ src/test/java/click/divichartnext/util/JwtUtilTest.java
@@ -3,7 +3,10 @@ package click.divichartnext.util;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class JwtUtilTest {
 
